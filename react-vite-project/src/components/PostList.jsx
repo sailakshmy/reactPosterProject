e7b6de47980590cd2778662967c43243115aad40
sailakshmy <sailakshmy94@gmail.com>
2024-09-23 07:34:43 +0400

    Added the scripts and files to create a backend server for the project

--- react-vite-project/src/components/PostList.jsx
+++ react-vite-project/src/components/PostList.jsx
@@ -22,6 +22,13 @@ const PostList = ({ modalIsVisible, hideModalHandler }) => {
 
   function addPosts(postData) {
     // setPosts([postData, ...posts]);
+    fetch('http://localhost:8080/posts',{
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
